bab77e418547686dae357007d5a879d046782ab3
CaptainHarrie <butchlinkle@gmail.com>
2025-01-27 16:27:42 +0000

    swap condition for happy path

--- src/__models__/articles.js
+++ src/__models__/articles.js
@@ -3,8 +3,8 @@ exports.selectArticleById = (article_id) => {
   return db
     .query("SELECT * FROM articles WHERE article_id = $1", [article_id])
     .then(({ rows }) => {
-      if (rows.length === 1) {
-        return rows[0];
-      } else return Promise.reject({ status: 404, msg: "Not Found" });
+      if (rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Not Found" });
+      } else return rows[0];
     });
 };
