835af9c8160ac383c67f673fe8e21e0533d5fd19
CaptainHarrie <butchlinkle@gmail.com>
2025-01-27 15:26:21 +0000

    write controller & model for endpoint GET/api/articles/:article_id

---
+++ src/__controllers__/articles.js
@@ -0,0 +1,12 @@
+const { selectArticleById } = require("../__models__/articles");
+
+exports.getArticleById = (request, response, next) => {
+  const { article_id } = request.params;
+  selectArticleById(article_id)
+    .then((article) => {
+      response.status(200).send({ article });
+    })
+    .catch((error) => {
+      next(error);
+    });
+};
