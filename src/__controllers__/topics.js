45e5b5ebeef4c6763eeaf9032cbebeff0e6d6020
Harrie <54632985+captainharrie@users.noreply.github.com>
2025-01-27 13:33:57 +0000

    Revert "3 GET/api/topics"

--- src/__controllers__/topics.js
+++
@@ -1,8 +0,0 @@
-const { selectTopics } = require("../__models__/topics");
-
-exports.getTopics = (request, response, next) => {
-  selectTopics().then((topics) => {
-    console.log(topics);
-    response.status(200).send({ topics });
-  });
-};
