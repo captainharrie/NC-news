0700b2256255ac61d6d26cfcd380e4629bb8d698
CaptainHarrie <butchlinkle@gmail.com>
2025-01-28 11:38:58 +0000

    create model and controller for GET /api/articles

--- src/__models__/articles.js
+++ src/__models__/articles.js
@@ -8,3 +8,15 @@ exports.selectArticleById = (article_id) => {
       } else return rows[0];
     });
 };
+
+exports.selectArticles = () => {
+  return db
+    .query(
+      `SELECT article_id, title, topic, author, created_at, votes, article_img_url, 
+        (SELECT COUNT(*)::INT FROM comments WHERE article_id = articles.article_id) AS comment_count
+        FROM articles ORDER BY articles.created_at DESC`
+    )
+    .then(({ rows }) => {
+      return rows;
+    });
+};
